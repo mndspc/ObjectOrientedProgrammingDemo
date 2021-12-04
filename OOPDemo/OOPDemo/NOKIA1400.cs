ecbdbae97b9c303a591f2fee53fa829dc41bbf28
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-12-04 19:30:36 +0530

    Push new Project
    this project contains demos about oop

---
+++ OOPDemo/OOPDemo/NOKIA1400.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace OOPDemo
+{
+    //  This is Single Inheritance
+    class NOKIA1400:MobilePhone
+    {
+        public NOKIA1400()
+        {
+            Console.WriteLine("Default constructor of NOKIA1400");
+        }
+        public NOKIA1400(int modelNo, string modelName, int year) :base(modelNo,modelName,year)
+        {
+        Console.WriteLine($"Model No={modelNo}\nModelName={modelName}\nYear={year}");
+        }
+        public string MP3()
+        {
+            return "MP3() calling from NOKIA1400";
+        }
+    }
+}
---
+++ OOPDemo/OOPDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace OOPDemo
+{
+    //  This program demo. how to use single inheritance.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NOKIA1400 nokia1400 = new NOKIA1400();
+            NOKIA1400 nokia1400 = new NOKIA1400(123,"NOKIA1400",2010);
+            //nokia1400.ModelNo = 123;
+            //nokia1400.ModelName = "NOKIA1400";
+            //nokia1400.Year = 2005;
+
+            //Console.WriteLine(nokia1400.AboutMobilePhone());
+            Console.WriteLine(nokia1400.Calling());
+            Console.WriteLine(nokia1400.SMS());
+            Console.WriteLine(nokia1400.MP3());
+            Console.ReadLine();
+        }
+    }
+}
